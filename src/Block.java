2b19c97ca62d44a25c237084b3528989f3ae72cd
AnshBalar429 <anshbalar2910@gmail.com>
2025-05-20 23:54:24 +0530

    Add Block

---
+++ src/Block.java
@@ -0,0 +1,21 @@
+import java.util.Date;
+
+public class Block {
+    public String hash;
+    public String previousHash;
+    public String data;
+    public Long timestamp;
+
+    public String calculateHash() {
+        String hash = StringUtil.applySHA256(this.previousHash
+                + Long.toString(this.timestamp) + this.data);
+        return hash;
+    }
+
+    public Block(String data, String previousHash) {
+        this.previousHash = previousHash;
+        this.data = data;
+        this.timestamp = new Date().getTime();
+        this.hash = calculateHash();
+    }
+}
