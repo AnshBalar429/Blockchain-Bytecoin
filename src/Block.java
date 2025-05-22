2a6627b926eb9e3af32e7e3e828638dca9566bf8
AnshBalar429 <anshbalar2910@gmail.com>
2025-05-22 11:24:35 +0530

    Add mineBlock

--- src/Block.java
+++ src/Block.java
@@ -19,4 +19,14 @@ public class Block {
         this.timestamp = new Date().getTime();
         this.hash = calculateHash();
     }
+
+    public void mineBlock(int difficulty) {
+        String target = new String(new char[difficulty]).replace('\0', '0');
+        while (!hash.substring(0, difficulty).equals(target)) {
+            nonce++;
+            hash = calculateHash();
+        }
+        System.out.println("Block Mined!!! : " + hash);
+    }
+
 }
